67e20529f44484153a6c4145138d5ed176efaea2
wangshuodong <wawngshuodong2004@163.com>
2017-07-20 16:58:33 +0800

    Initial commit

---
+++ src/test/java/com/eliteams/quick4j/test/service/UserServiceTest.java
@@ -0,0 +1,37 @@
+package com.eliteams.quick4j.test.service;
+
+import com.eliteams.quick4j.core.feature.test.TestSupport;
+import com.eliteams.quick4j.core.util.ApplicationUtils;
+import com.eliteams.quick4j.web.model.User;
+import com.eliteams.quick4j.web.service.UserService;
+import org.junit.Test;
+
+import javax.annotation.Resource;
+import java.util.Date;
+
+public class UserServiceTest extends TestSupport {
+
+    @Resource
+    private UserService userService;
+
+    @Test
+    public void test_insert() {
+        User model = new User();
+        model.setUsername("starzou");
+        model.setPassword(ApplicationUtils.sha256Hex("123456"));
+        model.setCreateTime(new Date());
+        userService.insert(model);
+    }
+
+//    @Test
+    public void test_10insert() {
+        for (int i = 0; i < 10; i++) {
+            User model = new User();
+            model.setUsername("starzou" + i);
+            model.setPassword(ApplicationUtils.sha256Hex("123456"));
+            model.setCreateTime(new Date());
+            userService.insert(model);
+        }
+    }
+
+}
